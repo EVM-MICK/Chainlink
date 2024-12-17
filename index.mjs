98ed0684f1be0d56a3d19a4c14aeb5db15138db0
EVM-MICK <mybinancesetup@gmail.com>
2024-12-17 21:05:34 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -57,14 +57,14 @@ let lastGasPriceFetch = 0; // Timestamp of the last gas price fetch
 const cacheDuration = 5 * 60 * 1000; // 5 minutes
 const cache = new Map();
 const FALLBACK_TOKENS = [
-  { address: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9", symbol: "USDT", decimals: 6, name: "Tether USD" },
-  { address: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8", symbol: "USDC.e", decimals: 6, name: "USD Coin (Arb1)" },
-  { address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", symbol: "DAI", decimals: 18, name: "Dai Stablecoin" },
-  { address: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1", symbol: "WETH", decimals: 18, name: "Wrapped Ether" },
-  { address: "0x2f2a2543b76a4166549f7aab2e75bef0f6acb6de", symbol: "WBTC", decimals: 8, name: "Wrapped Bitcoin" },
-  { address: "0xba5ddf906d8bbf63d4095028c164e8243b77c77d", symbol: "AAVE", decimals: 18, name: "Aave Token" },
-  { address: "0xf97f4df75117a78c1a5a0dbb814af92458539fb4", symbol: "LINK", decimals: 18, name: "Chainlink Token" },
-  { address: "0x912ce59144191c1204e64559fe8253a0e49e6548", symbol: "ARB", decimals: 18, name: "Arbitrum" },
+  { address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", symbol: "USDT", decimals: 6, name: "Tether USD" },
+  { address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", symbol: "USDC.e", decimals: 6, name: "USD Coin (Arb1)" },
+  { address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", symbol: "DAI", decimals: 18, name: "Dai Stablecoin" },
+  { address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", symbol: "WETH", decimals: 18, name: "Wrapped Ether" },
+  { address: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", symbol: "WBTC", decimals: 8, name: "Wrapped Bitcoin" },
+  { address: "0xba5DdD1f9d7F570dc94a51479a000E3BCE967196", symbol: "AAVE", decimals: 18, name: "Aave Token" },
+  { address: "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4", symbol: "LINK", decimals: 18, name: "Chainlink Token" },
+  { address: "0x912CE59144191C1204E64559FE8253a0e49E6548", symbol: "ARB", decimals: 18, name: "Arbitrum" },
 ];
 // Contract configuration
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;  // Your deployed contract address
