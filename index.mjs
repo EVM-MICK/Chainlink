01a744edc7b93a582c90ff1a78e63f79b0774930
EVM-MICK <mybinancesetup@gmail.com>
2024-12-19 01:55:46 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -35,6 +35,7 @@ const apiQueue = new PQueue({
 
 // Define the CAPITAL as $100,000 USDC with 6 decimals (to match USDC decimal places)
 const CAPITAL = new BigNumber(100000).shiftedBy(6);   // $100,000 in USDC (6 decimals)
+const PATHFINDER_API_URL = "https://api.1inch.dev/swap/v6.0/42161";
 const PROFIT_THRESHOLD = CAPITAL.multipliedBy(0.005);  // Equivalent to 0.5% profit
 const MINIMUM_PROFIT_THRESHOLD = new BigNumber(500).shiftedBy(6);  // Minimum profit threshold $500 (6 decimals)
 // Optional: Setting a higher threshold for "critical" profits
