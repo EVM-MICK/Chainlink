088f9ff4e0beb046a0b0ae8a9c9675a85a87cc40
EVM-MICK <mybinancesetup@gmail.com>
2025-01-07 22:11:43 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -20,13 +20,13 @@ const web3 = new Web3(process.env.INFURA_URL);
 //const redis = new Redis(process.env.REDIS_URL); // Redis for distributed caching
 const provider = new JsonRpcProvider(process.env.INFURA_URL);
 const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
-const permit2Contract = new Contract(PERMIT2_ADDRESS, permit2Abi, wallet);
 const redisClient = new Redis(process.env.REDIS_URL); // Initialize with your Redis URL
 const setAsync = promisify(redisClient.set).bind(redisClient);
 const getAsync = promisify(redisClient.get).bind(redisClient)
 const REDIS_TTL = 60; // Cache data for 1 minute
 const API_BASE_URL = `https://api.1inch.dev/swap/v6.0/${CHAIN_ID}`;
 const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3"; // Permit2 contract address
+const permit2Contract = new Contract(PERMIT2_ADDRESS, permit2Abi, wallet);
 const API_KEY = process.env.ONEINCH_API_KEY; // Set 1inch API Key in .env
 // Constants and Configuration
 const GO_BACKEND_URL = process.env.GO_BACKEND_URL || "http://localhost:8080"; // Go service endpoint
