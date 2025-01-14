5bfdc0785b9211a61f1c40bf14381ea8bc3216a0
EVM-MICK <mybinancesetup@gmail.com>
2025-01-14 18:27:26 +0100

    Update main.go

--- go-service/main.go
+++ go-service/main.go
@@ -3524,19 +3524,15 @@ func wsBroadcastManager() {
 
 func enableCORS(next http.HandlerFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        // Set CORS headers
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-        // Handle preflight (OPTIONS) requests
-        if r.Method == http.MethodOptions {
-            log.Println("Handling CORS preflight request")
+        
+        if r.Method == "OPTIONS" {
             w.WriteHeader(http.StatusOK)
             return
         }
 
-        // Call the next handler for other methods
         next(w, r)
     }
 }
