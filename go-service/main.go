502cf61576a5b0c0390a9ea68bdcb3276ad79955
EVM-MICK <mybinancesetup@gmail.com>
2025-01-26 09:41:52 +0100

    Update main.go

--- go-service/main.go
+++ go-service/main.go
@@ -25,7 +25,7 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-        "github.com/redis/go-redis/v8"
+        "github.com/go-redis/redis/v8"
         "github.com/ethereum/go-ethereum/rpc"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
