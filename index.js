48779acbb9ae8c2624be5d9fcaf7535e2343aa89
EVM-MICK <mybinancesetup@gmail.com>
2025-02-27 19:07:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -345,16 +345,15 @@ function log(message, level = 'info') {
 }
 
 // Connect to Redis
-await redisClient.connect();
-// (async () => {
-//   try {
-//     await redisClient.connect();
-//     console.log('Connected to Redis');
-//   } catch (err) {
-//     console.error('Error connecting to Redis:', err);
-//     process.exit(1);
-//   }
-// })();
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis');
+  } catch (err) {
+    console.error('Error connecting to Redis:', err);
+    process.exit(1);
+  }
+})();
 
 redisClient.on('error', (err) => {
   console.error('Redis connection error:', err);
